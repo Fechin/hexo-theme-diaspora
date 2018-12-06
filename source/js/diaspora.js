fb7039d0fe9ec128efa7a1a059669517fa808ebe
Fechin Li <mr.fechin@gmail.com>
2018-12-06 11:44:20 +0800

    Fixed Gtalk中文路径的问题，感谢@vipcolud

--- source/js/diaspora.js
+++ source/js/diaspora.js
@@ -472,7 +472,7 @@ $(function() {
                   repo: comment.data('r'),
                   owner: comment.data('o'),
                   admin: comment.data('a'),
-                  id: location.pathname,
+                  id: decodeURI(window.location.pathname),
                   distractionFreeMode: comment.data('d')
                 })
                 $(".comment").removeClass("link")
